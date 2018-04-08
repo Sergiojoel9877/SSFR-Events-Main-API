d3944be9f513e69cd5b3192c22c845fe18b1d503
Sergio Joel Ferreras Batista <sergio.ferreras@isqwebsite.com>
2018-04-07 22:32:52 -0400

    Feat: AppDbInitializer creado. Feat: DBRepository creado.

---
+++ SSFR_MainAPI/Data/AppDbContextInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSFR_MainAPI.Data
+{
+    public class AppDbContextInitializer
+    {
+        public static void Initialize(AppDbContext context) {
+
+            context.Database.EnsureCreated();
+
+        } 
+    }
+}
